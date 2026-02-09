ff70f6d04f2dd7c6d1e379a04bc3e4d948110165
Speechify <peterz@speechify.com>
2026-02-09 10:31:50 +0100

    Update Package.swift for modern SPM support (swift-tools-version: 6.2)

--- Package.swift
+++ Package.swift
@@ -1,5 +1,26 @@
-import PackageDescription
+  // swift-tools-version: 6.2
 
-let package = Package(
-  name: "Locksmith"
-)
+  import PackageDescription
+
+  let package = Package(
+      name: "Locksmith",
+      platforms: [
+          .iOS(.v13),
+          .macOS(.v10_15),
+          .watchOS(.v6),
+          .tvOS(.v13)
+      ],
+      products: [
+          .library(
+              name: "Locksmith",
+              targets: ["Locksmith"]
+          )
+      ],
+      targets: [
+          .target(
+              name: "Locksmith",
+              path: "Source",
+              exclude: ["Info.plist", "Locksmith.h"]
+          )
+      ]
+  )
